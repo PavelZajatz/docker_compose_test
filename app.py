6edc2a50c65684d0e92fcd196aeeecd5f5850c6a
Pavluha44 <52718431+Pavluha44@users.noreply.github.com>
2020-11-03 22:32:47 +0200

    added flask app and dockers

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from flask import Flask
 
 app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
+# In this example, redis is the hostname of the redis container on the application’s network.
+# We use the default port for Redis, 6379.
+
 
 def get_hit_count():
     retries = 5
@@ -20,4 +23,4 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Hello from Docker! I have been seen {} times.\n'.format(count)
+    return 'Hello from Docker!!!! I have been seen {} times.\n'.format(count)
